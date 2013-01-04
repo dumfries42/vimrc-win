0a71ab25441906af733561da07b220c0c5d9f9d5
MinmingQian <qianminming@gmail.com>
2013-01-04 15:18:50 +0800

    cc

--- auto-install.sh
+++ auto-install.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
-VIMHOME=~/.vim 
+VIMHOME=~/.vim
 
 warn() {
-        echo "$1" >&2
-    }
+    echo "$1" >&2
+}
 
 die() {
-        warn "$1"
-        exit 1
-    }
+    warn "$1"
+    exit 1
+}
 
 [ -e "$VIMHOME/vimrc" ] && die "$VIMHOME/vimrc already exists."
 [ -e "~/.vim" ] && die "~/.vim already exists."
 [ -e "~/.vimrc" ] && die "~/.vimrc already exists."
 
-git clone git://github.com/fofore/vimrc.git "$VIMHOME"
+git clone git://github.com/vgod/vimrc.git "$VIMHOME"
 cd "$VIMHOME"
 git submodule update --init
 
 ./install-vimrc.sh
 
-echo "fofore's vimrc is installed."
+cd bundle/command-t/ruby/command-t
+(ruby extconf.rb && make) || warn "Can't compile Command-T."
+
+echo "vgod's vimrc is installed."
