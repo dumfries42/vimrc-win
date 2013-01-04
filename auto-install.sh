1ff384f378c49fdba88ab71c1b2cfb9151365f9a
MinmingQian <qianminming@gmail.com>
2013-01-04 15:17:00 +0800

    cc

--- auto-install.sh
+++ auto-install.sh
@@ -3,12 +3,12 @@ VIMHOME=~/.vim
 
 warn() {
         echo "$1" >&2
-}
+    }
 
 die() {
         warn "$1"
         exit 1
-}
+    }
 
 [ -e "$VIMHOME/vimrc" ] && die "$VIMHOME/vimrc already exists."
 [ -e "~/.vim" ] && die "~/.vim already exists."
